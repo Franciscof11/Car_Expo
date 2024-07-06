2b83546ebe2069d6c46664e9df91bf5831e3d60a
Francisco Gabriel <franciscogabrielf11@gmail.com>
2024-07-06 13:24:42 -0300

    feat: home page

--- lib/modules/cars/presentation/home_page/home_page.dart
+++ lib/modules/cars/presentation/home_page/home_page.dart
@@ -1,25 +1,178 @@
-import 'package:car_expo/modules/cars/data/cars_repository.dart';
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class HomePage extends StatelessWidget {
+import 'package:car_expo/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../data/cars_repository.dart';
+import '../widgets/brand_list.dart';
+import '../widgets/cars_item.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final carsRepository = CarsRepository();
+  HomePageState createState() => HomePageState();
+}
 
-    getCars() async {
-      final cars = await carsRepository.getAllCars();
+class HomePageState extends State<HomePage> {
+  final carsRepository = CarsRepository();
 
-      print(cars);
+  getCars() async {
+    final cars = await carsRepository.getAllCars();
+
+    print(cars);
+  }
+
+  int _selectedPageIndex = 0;
+
+  void setSelectedIndex(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  bool _isVisible = true;
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.userScrollDirection == ScrollDirection.reverse) {
+      if (_isVisible) {
+        setState(() {
+          _isVisible = false;
+        });
+      }
+    } else if (_scrollController.position.userScrollDirection == ScrollDirection.forward) {
+      if (!_isVisible) {
+        setState(() {
+          _isVisible = true;
+        });
+      }
     }
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: GestureDetector(
-          onTap: () => getCars(),
-          child: const Text('KATIAU'),
+      backgroundColor: AppColors.whiteIce,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      style: GoogleFonts.montserrat(
+                        fontSize: 26,
+                        color: Colors.black87,
+                      ),
+                      children: const <TextSpan>[
+                        TextSpan(text: 'Olá,', style: TextStyle(fontWeight: FontWeight.bold)),
+                        TextSpan(text: ' Francisco!'),
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.search,
+                      size: 30,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            const BrandList(),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 0, horizontal: 23),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Carros Disponíveis",
+                          style: GoogleFonts.montserrat(
+                            fontSize: 15,
+                            color: AppColors.black87,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.sort),
+                        )
+                      ],
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        controller: _scrollController,
+                        itemCount: 3,
+                        itemBuilder: (context, index) {
+                          return const CarItem();
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
+      ),
+      bottomNavigationBar: AnimatedOpacity(
+        opacity: _isVisible ? 1.0 : 0.0,
+        duration: const Duration(milliseconds: 300),
+        child: _isVisible
+            ? Container(
+                margin: EdgeInsets.only(right: 20, left: 20, bottom: Platform.isAndroid ? 15 : 0),
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF17203A).withOpacity(0.8),
+                  borderRadius: const BorderRadius.all(Radius.circular(24)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color(0xFF17203A).withOpacity(0.3),
+                      offset: const Offset(0, 20),
+                      blurRadius: 20,
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.grid_view, color: Colors.white),
+                      onPressed: () {
+                        setState(() {
+                          _selectedPageIndex = 0;
+                        });
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.person, color: Colors.white),
+                      onPressed: () {
+                        setState(() {
+                          _selectedPageIndex = 1;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              )
+            : const SizedBox.shrink(),
       ),
     );
   }
---
+++ lib/modules/cars/presentation/widgets/brand_list.dart
@@ -0,0 +1,147 @@
+import 'package:car_expo/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BrandList extends StatefulWidget {
+  const BrandList({
+    super.key,
+  });
+
+  @override
+  BrandListState createState() => BrandListState();
+}
+
+class BrandListState extends State<BrandList> {
+  int _brandCategoryIndex = 0;
+
+  var brandList = [
+    {
+      "name": "all",
+      "icon": "",
+    },
+    {
+      "name": "chevrolet",
+      "icon": "assets/icons/chevrolet_icon.png",
+    },
+    {
+      "name": "volks",
+      "icon": "assets/icons/volks_icon.png",
+    },
+    {
+      "name": "toyota",
+      "icon": "assets/icons/toyota_icon.png",
+    },
+  ];
+
+  void setBrandIndex(int currentIndex) {
+    setState(() {
+      _brandCategoryIndex = currentIndex;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Text(
+            "Marcas",
+            style: GoogleFonts.montserrat(
+              fontSize: 15,
+              color: AppColors.black87,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 100,
+          width: double.infinity,
+          child: ListView.builder(
+            itemCount: brandList.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return GestureDetector(
+                  onTap: () {
+                    setBrandIndex(index);
+                    print(brandList[index]["name"]!);
+                  },
+                  child: BrandItem(
+                    imgAsset: brandList[index]["icon"]!,
+                    index: 0,
+                    isSelected: (_brandCategoryIndex == index) ? true : false,
+                  ),
+                );
+              } else {
+                return GestureDetector(
+                  onTap: () {
+                    setBrandIndex(index);
+                    print(brandList[index]["name"]!);
+                  },
+                  child: BrandItem(
+                    imgAsset: brandList[index]["icon"]!,
+                    index: index,
+                    isSelected: (_brandCategoryIndex == index) ? true : false,
+                  ),
+                );
+              }
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class BrandItem extends StatelessWidget {
+  final String imgAsset;
+  final int index;
+  final bool isSelected;
+
+  const BrandItem({
+    super.key,
+    required this.imgAsset,
+    required this.index,
+    required this.isSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: (index == 0) ? const EdgeInsets.fromLTRB(20, 12, 10, 12) : const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+      padding: (index == 0) ? const EdgeInsets.symmetric(vertical: 20, horizontal: 25) : const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: (isSelected)
+            ? Colors.blue
+            : (index == 0)
+                ? Colors.black12
+                : Colors.white,
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 7,
+            spreadRadius: 0.5,
+          ),
+        ],
+      ),
+      child: (index == 0)
+          ? Center(
+              child: Text(
+                "Todas",
+                style: GoogleFonts.montserrat(
+                  fontSize: 15,
+                  color: AppColors.black87,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            )
+          : Image.asset(
+              imgAsset,
+              width: 70,
+            ),
+    );
+  }
+}
