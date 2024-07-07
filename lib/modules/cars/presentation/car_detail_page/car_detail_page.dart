8c152fa2218e20a4f0b8c41e5c2838b8417ff77a
Francisco Gabriel <franciscogabrielf11@gmail.com>
2024-07-07 15:19:26 -0300

    refac: car details page

--- lib/modules/cars/presentation/car_detail_page/car_detail_page.dart
+++ lib/modules/cars/presentation/car_detail_page/car_detail_page.dart
@@ -1,8 +1,12 @@
+import 'package:car_expo/modules/cars/domain/car.dart';
+import 'package:car_expo/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class CarDetailPage extends StatefulWidget {
-  const CarDetailPage({super.key});
+  final Car car;
+  const CarDetailPage({super.key, required this.car});
 
   @override
   CarDetailPageState createState() => CarDetailPageState();
@@ -11,114 +15,165 @@ class CarDetailPage extends StatefulWidget {
 class CarDetailPageState extends State<CarDetailPage> {
   @override
   Widget build(BuildContext context) {
+    NumberFormat formatter = NumberFormat('#.000');
+    String carValor = formatter.format(widget.car.valor);
+
+    Widget renderImageCar(int modeloId) {
+      switch (modeloId) {
+        case 12:
+          return Image.asset(
+            "assets/images/onix.png",
+            width: 500,
+            height: 500,
+          );
+
+        case 14:
+          return Image.asset(
+            "assets/images/jetta.png",
+            width: 230,
+            height: 230,
+          );
+
+        case 79:
+          return Image.asset(
+            "assets/images/sw4.png",
+            width: 400,
+            height: 400,
+            fit: BoxFit.cover,
+          );
+
+        default:
+          return Container();
+      }
+    }
+
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
           automaticallyImplyLeading: false,
-          elevation: 0,
           backgroundColor: Colors.black87,
-          title: IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: const Icon(
-              Icons.arrow_back,
-              color: Colors.white60,
-              size: 30,
+          title: Padding(
+            padding: const EdgeInsets.only(top: 20, left: 10),
+            child: GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: const Icon(
+                Icons.arrow_back_ios,
+                size: 40,
+                color: AppColors.whiteIce,
+              ),
             ),
           ),
         ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TopMenuAndShowcase(),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(20, 15, 0, 10),
-              child: Text(
-                "Specifications",
-              ),
-            ),
             Container(
               width: double.infinity,
-              height: 100,
+              height: 300,
+              decoration: const BoxDecoration(
+                color: AppColors.black87,
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(50),
+                  bottomLeft: Radius.circular(50),
+                ),
+              ),
+              child: renderImageCar(widget.car.modeloId),
+            ),
+            const SizedBox(height: 25),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 22),
+              child: Text(
+                "Ficha Técnica",
+                style: GoogleFonts.montserrat(
+                  fontSize: 24,
+                  color: AppColors.black87,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 22),
               child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
+                child: Column(
                   children: [
                     Container(
-                      margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                      padding: EdgeInsets.symmetric(horizontal: 50),
-                      decoration: BoxDecoration(color: Colors.black87, borderRadius: BorderRadius.circular(20)),
-                      child: Center(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Image.asset(
-                              "assets/icons/ic_speedometer.png",
-                              height: 30,
-                              width: 30,
+                      height: 55,
+                      decoration: BoxDecoration(color: Colors.black87, borderRadius: BorderRadius.circular(12)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const SizedBox(width: 22),
+                          Text(
+                            "Modelo: ${widget.car.nomeModelo}",
+                            style: GoogleFonts.montserrat(
+                              fontSize: 20,
+                              color: AppColors.whiteIce,
+                              fontWeight: FontWeight.w600,
                             ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            RichText(
-                              text: TextSpan(
-                                style: GoogleFonts.montserrat(
-                                  fontSize: 16,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                                children: const <TextSpan>[
-                                  TextSpan(text: '322'),
-                                  TextSpan(
-                                    text: ' km/h',
-                                    style: TextStyle(
-                                      color: Colors.white38,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
+                    const SizedBox(height: 25),
                     Container(
-                      margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                      padding: EdgeInsets.symmetric(horizontal: 50),
-                      decoration: BoxDecoration(color: Colors.black87, borderRadius: BorderRadius.circular(20)),
-                      child: Center(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            RotatedBox(
-                              quarterTurns: 1,
-                              child: Image.asset(
-                                "assets/icons/ic_cartopview.png",
-                                height: 30,
-                                width: 30,
-                              ),
+                      height: 55,
+                      decoration: BoxDecoration(color: Colors.black87, borderRadius: BorderRadius.circular(12)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const SizedBox(width: 22),
+                          Text(
+                            "Ano de lançamento: ${widget.car.ano}",
+                            style: GoogleFonts.montserrat(
+                              fontSize: 20,
+                              color: AppColors.whiteIce,
+                              fontWeight: FontWeight.w600,
                             ),
-                            SizedBox(
-                              height: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    Container(
+                      height: 55,
+                      decoration: BoxDecoration(color: Colors.black87, borderRadius: BorderRadius.circular(12)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const SizedBox(width: 22),
+                          Text(
+                            "Combustível: ${widget.car.combustivel}",
+                            style: GoogleFonts.montserrat(
+                              fontSize: 20,
+                              color: AppColors.whiteIce,
+                              fontWeight: FontWeight.w600,
                             ),
-                            RichText(
-                              text: TextSpan(
-                                style: GoogleFonts.montserrat(
-                                  fontSize: 16,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w300,
-                                ),
-                                children: const <TextSpan>[
-                                  TextSpan(text: 'Liftback'),
-                                ],
-                              ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    Container(
+                      height: 55,
+                      decoration: BoxDecoration(color: Colors.black87, borderRadius: BorderRadius.circular(12)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const SizedBox(width: 22),
+                          Text(
+                            "Número de portas: ${widget.car.numPortas}",
+                            style: GoogleFonts.montserrat(
+                              fontSize: 20,
+                              color: AppColors.whiteIce,
+                              fontWeight: FontWeight.w600,
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
@@ -127,127 +182,46 @@ class CarDetailPageState extends State<CarDetailPage> {
             ),
           ],
         ),
-        bottomSheet: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                margin: EdgeInsets.only(left: 30),
-                child: RichText(
-                  text: TextSpan(
-                    style: GoogleFonts.montserrat(fontSize: 20, color: Colors.black87, fontWeight: FontWeight.w500),
-                    children: const <TextSpan>[
-                      TextSpan(text: '\$180'),
-                      TextSpan(text: '/day', style: TextStyle(color: Colors.black38)),
-                    ],
-                  ),
+        bottomSheet: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(left: 30),
+              child: Text(
+                'R\$$carValor',
+                style: GoogleFonts.montserrat(
+                  fontSize: 20,
+                  color: Colors.black87,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-              Container(
-                width: 170,
-                height: 60,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => CarDetailPage()));
-                  },
-                  child: Text(
-                    "EU QUERO",
-                    style: GoogleFonts.montserrat(fontWeight: FontWeight.w400, fontSize: 18),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    elevation: 0,
-                    shape: new RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                      ),
+            ),
+            SizedBox(
+              width: 170,
+              height: 60,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  elevation: 0,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
                     ),
                   ),
                 ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class TopMenuAndShowcase extends StatelessWidget {
-  const TopMenuAndShowcase({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 350,
-      decoration: BoxDecoration(
-        color: Colors.black87,
-        borderRadius: BorderRadius.only(
-          bottomRight: Radius.circular(50),
-          bottomLeft: Radius.circular(50),
-        ),
-      ),
-      child: Stack(
-        children: [
-          Positioned(
-            top: 70,
-            right: 20,
-            left: 20,
-            bottom: 0,
-            child: Image.asset(
-              "assets/images/tesla_1.png",
-              width: 300,
-            ),
-          ),
-          Positioned(
-            top: 0,
-            right: 0,
-            left: 0,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.fromLTRB(15, 0, 18, 0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 12),
-                        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                          boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 7, spreadRadius: 0.5)],
-                        ),
-                        child: Image.asset(
-                          "assets/icons/toyota_icon.png",
-                          width: 25,
-                          height: 25,
-                        ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Tesla Model S",
-                            ),
-                            Text(
-                              "2021",
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                child: Text(
+                  "EU QUERO",
+                  style: GoogleFonts.montserrat(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.white,
                   ),
                 ),
-              ],
-            ),
-          ),
-        ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
